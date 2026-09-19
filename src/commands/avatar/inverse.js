312e2de3c260944300f4f15caf207eb506c3c44a
Sayonara <141670764+sayohelloworld@users.noreply.github.com>
2026-09-19 09:48:18 +0200

    Update inverse.js

--- src/commands/avatar/inverse.js
+++ src/commands/avatar/inverse.js
@@ -26,9 +26,9 @@ module.exports = {
       const data = imageData.data;
 
       for (let i = 0; i < data.length; i += 4) {
-        data[i] = 255 - data[i];       // R
-        data[i + 1] = 255 - data[i+1]; // G
-        data[i + 2] = 255 - data[i+2]; // B
+        data[i] = 255 - data[i];      
+        data[i + 1] = 255 - data[i+1];
+        data[i + 2] = 255 - data[i+2];
       }
 
       ctx.putImageData(imageData, 0, 0);
